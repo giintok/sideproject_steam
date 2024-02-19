c1a54a7ec604e1b6a7cc59bc5538ea62f9e03814
김기인 <140138067+giintok@users.noreply.github.com>
2024-02-19 21:26:29 +0900

    Add files via upload
    2024-02-19
    데이터 변경으로 인한 시각화 수정 app.py도 수정

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 from page import intro
-from page import page_visual_metacritic as p1
-from page import page_visual_developers as p2
-from page import page_visual_tags as p3
-from page import page_recommendation_by_tags_ver as p4
-from page import page_recommendation_by_tags_ver02 as p5
-from page import page_recommendation_by_mbti as p6
+from page import page_visual_metacritic as pv1
+from page import page_visual_total_review as pv2
+from page import page_visual_tag_genres_count as pv3
+from page import page_recommendation_by_tags_top30 as pr4
+from page import page_recommendation_by_tags_select as pr5
 
 st.title('Side_Project')
 
@@ -21,17 +20,15 @@ if item == 'item0':
 elif item == 'item1':
     side_select = st.sidebar.selectbox('시각화를 선택하세요.', ['시각화1', '시각화2', '시각화3'])
     if side_select == '시각화1':
-        p1.app()
+        pv1.app()
     elif side_select == '시각화2':
-        p2.app()
+        pv2.app()
     elif side_select == '시각화3':
-        p3.app()
+        pv3.app()
    
 elif item == 'item2':
-    sub_item = st.sidebar.selectbox('추천 서비스 예시', ['추천1', '추천2', '추천3'])
+    sub_item = st.sidebar.selectbox('추천 서비스 예시', ['추천1', '추천2'])
     if sub_item == '추천1':
-        p4.app()
+        pr4.app()
     elif sub_item == '추천2':
-        p5.app()
-    elif sub_item == '추천3':
-        p6.app()
+        pr5.app()
